94260de5f6b944cd5841a23fc28717b4064d4c8a
Rodrigo <digo1209@gmail.com>
2023-04-03 10:45:04 -0300

    utilizando switch

---
+++ script.js
@@ -0,0 +1,16 @@
+let opcao = parseInt(prompt('Digite uma opcao: '));
+
+switch(opcao){
+    case 1:
+        document.write("Primeira opcao")
+    break;
+    case 2:
+        document.write("Segunda opcao")
+    break;
+    case 3:
+        document.write("Terceira opcao")
+    break;
+    default:
+        document.write("Nenhuma das opcoes validas")
+    break;
+}
