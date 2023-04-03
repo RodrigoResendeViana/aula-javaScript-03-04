cceaeed13a90e59ddadc7cfd7fb7073a46e8149a
Rodrigo <digo1209@gmail.com>
2023-04-03 11:21:00 -0300

    utilizando o for

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 let opcao = parseInt(prompt('Digite uma opcao: '));
 
 switch(opcao){
@@ -13,4 +14,30 @@ switch(opcao){
     default:
         document.write("Nenhuma das opcoes validas")
     break;
+}
+*/
+/*incremento e decremento
+let numero = 7
+document.write(numero)
+document.write("<br><br>")
+numero = numero + 1
+document.write(numero)
+numero = numero - 1
+document.write("<br><br>")
+document.write(numero)
+
+*/
+/* Uso do for
+for(let i = 0; i<=10000 ; i++){
+    document.write("Agora i vale " + i);
+    document.write("<br><br>")
+}
+
+*/
+
+let carros=["Gol","Fusca","HB20","Corsa"]
+
+for(let i=0 ; i < carros.length; i++){
+    document.write(carros[i])
+    document.write("<br><br>")
 }
